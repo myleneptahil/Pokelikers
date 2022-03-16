8f40de974711d2022522dd4008dbc9b513782487
myleneptahil <nentech08@gmail.com>
2022-03-16 15:43:12 +0800

    Create models

---
+++ pokelikers/database/migrations/2022_03_16_052728_create_liked_pokemons_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('liked_pokemons', function (Blueprint $table) {
+            $table->id();
+            $table->string('pokemon_name',1000);
+            $table->longText('like_reason')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('liked_pokemons');
+    }
+};
---
+++ pokelikers/database/migrations/2022_03_16_052756_create_hated_pokemons_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hated_pokemons', function (Blueprint $table) {
+            $table->id();
+            $table->string('pokemon_name',1000);
+            $table->longText('reason_hate',1000);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hated_pokemons');
+    }
+};
