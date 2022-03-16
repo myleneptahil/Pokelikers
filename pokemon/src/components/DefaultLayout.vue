794004ecd209991bad26b830c486f90c9b22a1de
myleneptahil <nentech08@gmail.com>
2022-03-16 12:52:18 +0800

    Add sign out

--- pokemon/src/components/DefaultLayout.vue
+++ pokemon/src/components/DefaultLayout.vue
@@ -1,24 +1,28 @@
-<!-- This example requires Tailwind CSS v2.0+ -->
 <template>
-  <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-gray-100">
-    <body class="h-full">
-    ```
-  -->
   <div class="min-h-full">
     <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between h-16">
           <div class="flex items-center">
             <div class="flex-shrink-0">
-              <img class="h-8 w-8" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
+              <img 
+                class="h-8 w-8" 
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" 
+                alt="Workflow" />
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
-                <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                <router-link 
+                    v-for="item in navigation" 
+                    :key="item.name" 
+                    :to = "item.to"
+                    active-class="bg-gray-900 text-white"
+                    :class="[
+                        this.$route.name === item.to.name 
+                        ? '' 
+                        : 'text-gray-300 hover:bg-gray-700 hover:text-white', 
+                        'px-3 py-2 rounded-md text-sm font-medium']" 
+                        >{{ item.name }}</router-link>
               </div>
             </div>
           </div>
@@ -39,8 +43,14 @@
                 </div>
                 <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
                   <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
-                      <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
+                    <MenuItem 
+                    v-slot="{ active }">
+                      <a 
+                      @click="logout"
+                      :class="[
+                        'block px-4 py-2 text-sm text-gray-700'
+                        ]"
+                        >Sign out</a>
                     </MenuItem>
                   </MenuItems>
                 </transition>
@@ -60,7 +70,18 @@
 
       <DisclosurePanel class="md:hidden">
         <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-          <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+          <router-link
+            v-for="item in navigation" 
+            :key="item.name"  
+            :to="item.to" 
+            active-class="bg-gray-900 text-white"
+            :class="[
+                this.$route.name === item.to.name
+                ? '' 
+                : 'text-gray-300 hover:bg-gray-700 hover:text-white', 
+                'block px-3 py-2 rounded-md text-base font-medium'
+                ]" 
+                >{{ item.name }}</router-link>
         </div>
         <div class="pt-4 pb-3 border-t border-gray-700">
           <div class="flex items-center px-5">
@@ -77,7 +98,21 @@
             </button>
           </div>
           <div class="mt-3 px-2 space-y-1">
-            <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700">{{ item.name }}</DisclosureButton>
+            <DisclosureButton 
+                as="a" 
+                @click="logout" 
+                class="
+                block 
+                px-3 
+                py-2 
+                rounded-md 
+                text-base 
+                font-medium 
+                text-gray-400 
+                hover:text-white 
+                hover:bg-gray-700
+                cursor-pointer"
+                >Sign out</DisclosureButton>
           </div>
         </div>
       </DisclosurePanel>
@@ -89,25 +124,14 @@
 
 <script>
 import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
+import { BellIcon, MenuIcon, XIcon } from '@heroicons/vue/outline';
+import {useStore} from 'vuex';
+import {computed} from 'vue';
+import {useRouter} from 'vue-router';
 
-const user = {
-  name: 'Tom Cook',
-  email: 'tom@example.com',
-  imageUrl:
-    'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-}
 const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
-]
-const userNavigation = [
-  { name: 'Your Profile', href: '#' },
-  { name: 'Settings', href: '#' },
-  { name: 'Sign out', href: '#' },
+  { name: 'Dashboard', to: { name: "Dashboard"} },
+  { name: 'Pokemon List', to: { name: "Pokemons"} },
 ]
 
 export default {
@@ -124,10 +148,19 @@ export default {
     XIcon,
   },
   setup() {
+    const store = useStore();
+    const router = useRouter();
+
+    function logout(){
+        store.commit('logout');
+        router.push({
+            name: 'Login'
+        });
+    }
     return {
-      user,
+      user: computed(() => store.state.user.data),
       navigation,
-      userNavigation,
+      logout,
     }
   },
 }
