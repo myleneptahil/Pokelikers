446be56f3ac77dd9c8847d42412508494c9c505b
myleneptahil <nentech08@gmail.com>
2022-03-17 14:43:13 +0800

    Add laravel authentication

--- pokelikers/routes/web.php
+++ pokelikers/routes/web.php
@@ -1,21 +1,24 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\PokemonListController;
-use App\Http\Controllers\PokemonPollsSurveyController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    echo "Please love Pokemon so that you can access this site.";
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-
-Route::get('/pokemonlist', [PokemonListController::class,'index']);
-
-Route::get('/profile', [ProfileController::class,'index']);
-
-Route::get('/pokemonpollssurvey', [PokemonPollsSurveyController::class,'index']);
+require __DIR__.'/auth.php';
